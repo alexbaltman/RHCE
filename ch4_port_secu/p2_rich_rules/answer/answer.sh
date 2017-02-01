89571b12095ed0d9e943b0fa1d480d95363a004f
Alex Altman <aaltman@cisco.com>
2017-02-01 00:30:23 -0500

    Updating answer file ch4p2

--- ch4_port_secu/p2_rich_rules/answer/answer.sh
+++ ch4_port_secu/p2_rich_rules/answer/answer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.6.66" service name="http" log prefix="NEW HTTP " level="notice" limit value="3/s" accept'
+firewall-cmd --reload
