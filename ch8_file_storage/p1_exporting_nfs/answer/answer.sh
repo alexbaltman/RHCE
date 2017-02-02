6ad94bf6aeab43449b3a123eb0abdbd7fde85f53
Alex Altman <aaltman@cisco.com>
2017-02-01 22:58:14 -0500

    fixed bug in answer file for root user

--- ch8_file_storage/p1_exporting_nfs/answer/answer.sh
+++ ch8_file_storage/p1_exporting_nfs/answer/answer.sh
@@ -3,7 +3,7 @@ systemctl start nfs-server
 systemctl enable nfs-server
 mkdir /nfsshare
 chown nfsnobody /nfsshare
-echo '/nfsshare host2(rw)' >> /etc/exports
+echo '/nfsshare host2(rw,no_root_squash)' >> /etc/exports
 exportfs -r
 firewall-cmd --permanent --add-service=nfs
 firewall-cmd --reload
