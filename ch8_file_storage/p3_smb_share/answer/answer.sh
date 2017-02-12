f934f6a4ad0a62d93a638a612b8d8cb7b8388085
Alex Altman <aaltman@cisco.com>
2017-02-12 18:37:52 -0500

    updating task to be a bit more specific and adding in users plus grp to host1 setup.yaml

--- ch8_file_storage/p3_smb_share/answer/answer.sh
+++ ch8_file_storage/p3_smb_share/answer/answer.sh
@@ -8,23 +8,18 @@ baseurl=http://mirror.centos.org/centos/7/os/x86_64
 EOF
 
 yum install -y samba policycoreutils-python
-groupadd -r marketing
 mkdir /smbshare
 chgrp marketing /smbshare
 chmod 2775 /smbshare
 semanage fcontext -a -t samba_share_t '/smbshare(/.*)?'
 restorecon -vvFR /smbshare
 sed -i 's/\tworkgroup = SAMBA/\tworkgroup = mycompany/g' /etc/samba/smb.conf
-# default: security = user 
-# default: passdb backend = tdbsam 
 echo "[smbshare]" >>/etc/samba/smb.conf
 echo "path = /smbshare" >>/etc/samba/smb.conf
 echo "write list = @marketing" >>/etc/samba/smb.conf
 
 yum install -y samba-client
-useradd -s /sbin/nologin -G marketing brian
 (echo "redhat"; echo "redhat") | smbpasswd -s -a brian
-useradd -s /sbin/nologin rob
 (echo "redhat"; echo "redhat") | smbpasswd -s -a rob
 systemctl start smb
 systemctl start nmb
