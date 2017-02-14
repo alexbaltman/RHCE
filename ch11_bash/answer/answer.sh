0a1798c146f1bda381c14b48059f79deb50676dd
Alex Altman <aaltman@cisco.com>
2017-02-14 17:04:29 -0500

    adding a partial answer here for ch11.

--- ch11_bash/answer/answer.sh
+++ ch11_bash/answer/answer.sh
@@ -1,3 +1,15 @@
-!#/bin/bash
+#!/bin/bash
 
-case $1
+if [ "$#" -ne 1 ]; then
+        echo 'input foo or bar only'>/dev/stderr
+        exit 1
+fi
+
+case $1 in
+    "foo")
+        echo "bar";;
+    "bar")
+        echo "foo";;
+    *)
+        echo 'input foo or bar only'>/dev/stderr && exit 1;;
+esac
